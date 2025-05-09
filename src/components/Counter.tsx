79a44783adab1c43bec423ea2bd91563956f5ada
refrain62 <nao_h13@hotmail.com>
2025-05-09 23:11:49 +0900

    useEffectはいつ実行されるのか

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,16 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Counter: React.FC = () => {
   const [count, setCount] = useState(0);
-  const handleClick = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const countUp = () => {
     setCount((prevCount) => prevCount + 1);
   };
 
+  // useEffect_1
+  useEffect(() => {
+    console.log("再レンダリングされる度に実行");
+  });
+  // useEffect_2
+  useEffect(() => {
+    console.log("初回レンダリング時に実行");
+  }, []);
+  // useEffect_3
+  useEffect(() => {
+    console.log("countが変更された時に実行");
+  }, [count]);
+
   return (
-    <div>
-      <p>カウントは: {count}</p>
-      <button onClick={handleClick}>Count up</button>
-    </div>
+    <>
+      {console.log("---レンダリング---")}
+
+      <div>
+        <p>カウントは: {count}</p>
+        <button onClick={countUp}>Count up</button>
+        <button onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? "Close" : "Open"} Modal
+        </button>
+      </div>
+    </>
   );
 }
 
